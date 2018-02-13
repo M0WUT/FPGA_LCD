78eb7fc5cd8fb7c374f5ea06ba0116a2e2752a34
M0WUT <danielmcgraw@btinternet.com>
2018-02-13 19:32:20 +0000

    Added SPI testbench

--- FPGA/Verilog/Tests/spi_test/spi_test.v
+++ FPGA/Verilog/Tests/spi_test/spi_test.v
@@ -1,28 +1,40 @@
-module FPGA_LCD_top
+module spi_test
 (
 	//Master clock
-	input i_clock,
+	input			i_clock50,
+	
+	
 	
 	//SPI
-	output		o_sen,
-	output		o_sck,
-	output		o_sdat,
-	input		i_sout
+	output			o_sen,
+	output			o_sck,
+	output			o_sdat,
+	input			i_sout,
+	output			o_spiTxBusy,
+	output			o_spiTxDone,
+	
+	//Blinky Light
+	output		o_blinkyLight
+
+	
 );
 
 reg[7:0]		r_spiTxData;
 reg[6:0]		r_spiTxAddress;
 reg				r_spiTxBegin;
+reg[25:0]	r_clockCounter;
+
+assign o_blinkyLight = r_clockCounter[25];
 
 spi SPI_TCVR
 (
 	//TX
-	.i_clock(i_clock),
+	.i_clock(i_clock50),
 	.i_txBegin(r_spiTxBegin),
 	.i_txAddress(r_spiTxAddress),
 	.i_txData(r_spiTxData),
-	.o_txBusy(w_spiTxBusy),
-	.o_txDone(w_spiTxDone),
+	.o_txBusy(o_spiTxBusy),
+	.o_txDone(o_spiTxDone),
 	
 	/*
 	//Rx
@@ -40,9 +52,11 @@ spi SPI_TCVR
 	.o_sdat(o_sdat) //MISO
 );
 
-always @(posedge i_clock)
+
+
+always @(posedge i_clock50)
 begin
-	if(!w_spiTxBusy)
+	if(!o_spiTxBusy)
 	begin
 		r_spiTxAddress <= 7'h12;
 		r_spiTxData <= 8'h34;
@@ -51,7 +65,7 @@ begin
 	else
 		r_spiTxBegin <= 0;
 
-
+	r_clockCounter <= r_clockCounter + 1;
 
 end
 
--- FPGA/Verilog/spi.v
+++ FPGA/Verilog/spi.v
@@ -47,13 +47,13 @@ assign o_txDone = (r_state == s_TXDONE);
 assign o_rxBusy = (r_state > s_TXDONE);
 assign o_rxDone = (r_state == s_RXDONE);
 
-assign o_sen = (r_state != s_IDLE);
+assign o_sen = (r_state == s_IDLE);
 assign w_clockEnable = (r_state != s_IDLE);
 
 /////////////////
 //Clock Divider//
 ////////////////
-parameter 	CLOCKS_PER_BIT = 30; //MUST BE EVEN
+parameter 	CLOCKS_PER_BIT = 50; //MUST BE EVEN
 reg[7:0]	r_clockCounter = 0;
 wire		w_clockEnable;
 
@@ -61,7 +61,9 @@ assign o_sck = (r_clockCounter > CLOCKS_PER_BIT[7:1]); //[7:1] is a divide by 2 
 //If clock is disabled or has overflowed, set r_clockCounter to 0. This also takes o_sck low which is its idle state
 
 reg[3:0] r_bitCounter = 0;
-always @(posedge i_clock) r_clockCounter <= w_clockEnable ? (r_clockCounter > CLOCKS_PER_BIT ? 0 : r_clockCounter + 1) : 0;	
+always @(posedge i_clock) r_clockCounter <= w_clockEnable ? (r_clockCounter > CLOCKS_PER_BIT ? 0 : r_clockCounter + 1) : 1; //Set it to 0 if clock disabled	
+//Disabled value is 1 as later routines check for r_clockCounter == 0 to detect falling edge, this means that they happened instantly when the clock became enabled
+//The timing error introduced is not significant as SPI is edge triggered so separation not important
 
 always @(posedge i_clock)
 begin
@@ -72,20 +74,21 @@ begin
 		if(i_rxBegin)
 		begin
 			r_state <= s_RXSENDING;
-			r_rxAddress <= {1'b0, i_rxAddress[6:0]};
-			r_bitCounter <= 4'd7; //1 read bit plus 6 address bits
+			r_rxAddress <= {1'b1, i_rxAddress[6:0]};
+			r_bitCounter <= 4'd6; //1 read bit plus 6 address bits so MSB is bit 6
 		end
 		else if(i_txBegin)
 		begin
 			r_state <= s_TXSENDING;
 			r_txData <= {1'b0, i_txAddress[6:0], i_txData[7:0]}; //concatenate start bit, address and data
-			r_bitCounter <= 4'd15; //16 bits in total (15:0)
+			r_bitCounter <= 4'd15; //16 bits in total, MSB is bit 15
 		end
 		else r_state <= s_IDLE;
 	end //case s_IDLE
 	
 	s_TXSENDING:
 	begin
+	//Note that the 
 		o_sdat <= r_txData[r_bitCounter];
 		if(r_clockCounter == 0)
 		begin
